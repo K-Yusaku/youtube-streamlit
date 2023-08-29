dc6461da6501c220561609c59a37ae43c59217ec
kawalabo <k.yusaku@kawamotolabo.com>
2023-08-29 17:27:36 +0900

    初回

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import time
-
 import streamlit as st
-import numpy as np
-import pandas as pd
-from PIL import Image
+# import numpy as np
+# import pandas as pd
+# from PIL import Image
 
 st.title('Streamlit 超入門')
 st.write('プログレスバーの表示')
